85584a4c879e870ca40f968594e69a1d0c4f4137
Damian Mroz <damianmroz141@gmail.com>
2024-04-17 17:22:25 +0200

    Change name of variable

--- task.py
+++ task.py
@@ -3,9 +3,9 @@ mydict_initial={"piekarnia": ["chleb","pączek","bułki"],
         "warzywniak": ["marchew", "seler","rukola"]}
 mydict2=dict(mydict_initial)
 print("Lista zakupów")
-a=0
+element=0
 for x,y in mydict_initial.items():
-    mydict2[x] = [a.capitalize() for a in mydict_initial[x]]
+    mydict2[x] = [element.capitalize() for element in mydict_initial[x]]
     print(f"Idę do {x.capitalize()} i kupuję tam {mydict2[x]}.")
-    a = a+len(mydict2[x])
-print(f"W sumie kupuję {a} produktów.")
+    element = element+len(mydict2[x])
+print(f"W sumie kupuję {element} produktów.")
