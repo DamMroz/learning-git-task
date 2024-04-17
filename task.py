e10a2df2566df453348b75235b007154870eb421
Damian Mroz <damianmroz141@gmail.com>
2024-04-17 17:15:05 +0200

    Change name of dictionary

--- task.py
+++ task.py
@@ -1,11 +1,11 @@
 # Task
-mydict={"piekarnia": ["chleb","pączek","bułki"],
+mydict_initial={"piekarnia": ["chleb","pączek","bułki"],
         "warzywniak": ["marchew", "seler","rukola"]}
-mydict2=dict(mydict)
+mydict2=dict(mydict_initial)
 print("Lista zakupów")
 a=0
-for x,y in mydict.items():
-    mydict2[x] = [a.capitalize() for a in mydict[x]]
+for x,y in mydict_initial.items():
+    mydict2[x] = [a.capitalize() for a in mydict_initial[x]]
     print(f"Idę do {x.capitalize()} i kupuję tam {mydict2[x]}.")
     a = a+len(mydict2[x])
 print(f"W sumie kupuję {a} produktów.")
